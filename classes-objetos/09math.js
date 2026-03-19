e3f7c8cbb13056e37cb546f49a5c8200796650d1
Lorena Gonçalves <lorena.b.goncalves@aluno.senai.br>
2026-03-19 08:51:06 -0300

    modify: modifiquei o codigo pra ele ficar otimizado, comentei o codigo antigo

--- classes-objetos/09math.js
+++ classes-objetos/09math.js
@@ -1,30 +1,42 @@
-// logica para sortear numeros entre 1 e 15
+// // logica para sortear numeros entre 1 e 15
 
-const gerarNumeroAleatorio = (numero) => {
+// const gerarNumeroAleatorio = (numero) => {
 
-    // math.random() gera um numero entre 1 e 0 (exclusivo)
-    let numeroAleatorio = Math.random;
-    console.log(`Sorteio : ${numeroAleatorio}`);
+//     // math.random() gera um numero entre 1 e 0 (exclusivo)
+//     let numeroAleatorio = Math.random;
+//     console.log(`Sorteio : ${numeroAleatorio}`);
 
-    //multiplicamos por 15 para obter um numero entre 0 e 14 (exclusivo)
-    numeroAleatorio *= numero;
-    console.log(`Multiplicar: ${numeroAleatorio}`);
+//     //multiplicamos por 15 para obter um numero entre 0 e 14 (exclusivo)
+//     numeroAleatorio *= numero;
+//     console.log(`Multiplicar: ${numeroAleatorio}`);
 
-    //math.floor() para arredondar para BAIXO um numero inteiro
-    numeroAleatorio = Math.floor(numeroAleatorio);
-    console.log(`Arredondar: ${numeroAleatorio}`);
+//     //math.floor() para arredondar para BAIXO um numero inteiro
+//     numeroAleatorio = Math.floor(numeroAleatorio);
+//     console.log(`Arredondar: ${numeroAleatorio}`);
 
-    //somamos 1 para obter um numero entre 1 e 15 (inclusivo)
-    numeroAleatorio += 1;
-    console.log(`Somar 1: ${numeroAleatorio}`);
+//     //somamos 1 para obter um numero entre 1 e 15 (inclusivo)
+//     numeroAleatorio += 1;
+//     console.log(`Somar 1: ${numeroAleatorio}`);
 
-    //retorna o numero aleatorio
-    return numeroAleatorio;
+//     //retorna o numero aleatorio
+//     return numeroAleatorio;
 
-}
+// }
 
-//atribuindo a função a uma variavel
-let numeroSorteado = gerarNumeroAleatorio(20);
+// //atribuindo a função a uma variavel
+// let numeroSorteado = gerarNumeroAleatorio(20);
 
-//exibindo o numero sorteado
-console.log(`Numero sorteado: ${numeroSorteado}`);
+// //exibindo o numero sorteado
+// console.log(`Numero sorteado: ${numeroSorteado}`);
+
+
+
+//--------------codigo otimizado-----------------
+
+const gerarNumeroAleatorio = (min = 1, max = 15) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+//exemplo de uso
+console.log(gerarNumeroAleatorio()); //gera um numero entre 1 e 15
+console.log(gerarNumeroAleatorio(10,15)); //gera um numero entre 5 e 10
