194e8de9eda8927bcf88001c33072a400ffa8d14
Lorena Gonçalves <lorena.b.goncalves@aluno.senai.br>
2026-03-20 15:00:10 -0300

    add: finalizando a interface

--- sorteio-numeros-aleatorios/numeros.js
+++ sorteio-numeros-aleatorios/numeros.js
@@ -25,12 +25,15 @@ const atualizarValorSlider = () =>{
     document.querySelector('.intervalo__valor--max').textContent = max;
 };
 
+
 //evento para atualizar o valor em tempo real
-sliderMin.addEventListener('click', atualizarValorSlider);
+sliderMin.addEventListener('input', atualizarValorSlider);
 sliderMax.addEventListener('input', atualizarValorSlider);
+
 
 //inicializar interface com o valores atuais
 atualizarValorSlider();
+
 
 //função para gerar um numero aleatorio
 const gerarNumeroAleatorio = (min,max) => {
@@ -39,9 +42,83 @@ const gerarNumeroAleatorio = (min,max) => {
     return numeroAleatorio;
 };
 
+
+//função para atualizar o texto da interface com o numero sorteado
 const atualizarTexto = (elemento, valor) => {
 
+    elemento.textContent = valor;
 };
+
+
+// api armazena dados que funcionam automaticamente
+//navegador vai colocar uma mensagem de texto na tela
+const criarItemHistorico = (numero) => {
+    const li = document.createElement('li');
+    li.textContent = numero;
+
+    li.addEventListener('click', () => {
+        navigator.clipboard.writeText(numero);
+    });
+
+    return li;
+};
+
+
+//função para gerenciar a lista de numeros sorteados
+const atualizarHistorico = (lista, item, limite) => {
+    //prepend pendura o item na primeira posição da lista
+    //append coloca no ultimo
+    lista.prepend(item);
+
+    //se a quantidade de linhas que estiverem na lista for maior do que o limite, ele vai remover a ultima lista
+    if (lista.children.length > limite) {
+        lista.removeChild(lista.lastChild);
+    }
+};
+
+
+//função para limpar o historico de sorteios
+const limparHistorico = () => {
+    //se for verdadeiro ele vai limpar, se falso ele nao vai limpar
+    if (confirm('Deseja realmente limpar o historico de sorteios?')) {
+        listaNumeros.textContent = '';
+        elementoNumero.textContent = '0';
+    }
+};
+
+
+botaoSortear.addEventListener('click', () => {
+    //primeiro verifico se o valor minimo é maior do que o valor maximo
+    const min = Number(sliderMin.value);
+    const max = Number(sliderMax.value);
+
+    if (min > max) {
+        mensagem.textContent = 'O valor minimo deve ser menor ou igual ao valor máximo.';
+        return;
+    }
+
+    mensagem.textContent = '';
+
+    //criei a variavel numero sorteado pra guardar a função que vai sortear
+    const numeroSorteado = gerarNumeroAleatorio(min,max);
+
+    //chamei a função atualizar texto e coloquei o elemento que eu capturei la em cima
+    //em seguida, coloquei o numero sorteado, os dois como parametro
+    atualizarTexto(elementoNumero, numeroSorteado);
+
+    //criei uma variavel item pra guardar o item historico
+    //na função atualizar historico as funções são os parametros pedidos por ela: lista, item e limite
+    const item = criarItemHistorico(numeroSorteado);
+    atualizarHistorico(listaNumeros,item,quantidadeNumerosRecentes);
+});
+
+botaoLimparHistorico.addEventListener('click', limparHistorico)
+
+
+
+
+
+
 
 
 
